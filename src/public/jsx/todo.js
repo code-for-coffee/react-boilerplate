c8e834a289fe9cab4a1c6c3b0346a62aba68e003
Bill <bill@billpatrianakos.me>
2016-04-12 10:06:42 -0500

    Update for this example.

--- src/public/jsx/todo.js
+++ src/public/jsx/todo.js
@@ -1,16 +1,9 @@
-// Chart Components
-// ================
-// This file defines the main
-// Chart component and all child components.
-
-
 // Require dependencies
 // --------------------
 var React     = require('react'),
     ReactDOM  = require('react-dom'),
     request   = require('superagent'),
     _         = require('lodash');
-
 
 
 ReactDOM.render(<div>Hello</div>, document.getElementById('component'));
